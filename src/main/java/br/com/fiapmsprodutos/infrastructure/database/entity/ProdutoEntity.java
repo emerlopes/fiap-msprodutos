db71f7b6da2070769c0d2ecb8f317a23ae7b8971
Emerson <lopes.ds@live.com>
2024-05-19 11:37:11 -0300

    chore(fiap-msprodutos) adicionando @Accessors

--- src/main/java/br/com/fiapmsprodutos/infrastructure/database/entity/ProdutoEntity.java
+++ src/main/java/br/com/fiapmsprodutos/infrastructure/database/entity/ProdutoEntity.java
@@ -6,10 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Getter
 @Setter
 @Entity
+@Accessors(chain = true)
 public class ProdutoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
